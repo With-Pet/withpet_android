61bc9fea83466ca0df9680901c77e445d4ab8064
Ji <hwichance@gmail.com>
2022-02-03 20:32:59 +0900

    :sparkles: feat: 바텀 네비게이션 바와 연결된 프래그먼트 생성
    body: 4가지 프래그먼트 생성
          graph를 통해 프래그먼트와 바텀 네비게이션 바 연결

---
+++ app/src/main/java/com/withpet/withpet_android/ui/fragments/AccountFragment.kt
@@ -0,0 +1,8 @@
+package com.withpet.withpet_android.ui.fragments
+
+import androidx.fragment.app.Fragment
+import com.withpet.withpet_android.R
+
+class AccountFragment : Fragment(R.layout.fragment_account) {
+
+}
---
+++ app/src/main/java/com/withpet/withpet_android/ui/fragments/ChatFragment.kt
@@ -0,0 +1,8 @@
+package com.withpet.withpet_android.ui.fragments
+
+import androidx.fragment.app.Fragment
+import com.withpet.withpet_android.R
+
+class ChatFragment : Fragment(R.layout.fragment_chat) {
+
+}
