f2d343545d608ebf69ec8a314b960f5b1513cc3c
Ji <hwichance@gmail.com>
2022-02-03 17:30:01 +0900

    :sparkles: feat: 바텀 네비게이션 바 추가
    body: 바텀 네비게이션 바에 들어갈 아이콘 추가
          fab가 중앙에 위치한 바텀 네비게이션 바 추가

--- app/src/main/java/com/withpet/withpet_android/MainActivity.kt
+++ app/src/main/java/com/withpet/withpet_android/MainActivity.kt
@@ -2,10 +2,18 @@ package com.withpet.withpet_android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.withpet.withpet_android.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.mainBottomNavigationView.background = null
+        binding.mainBottomNavigationView.menu.getItem(2).isEnabled = false
     }
 }
